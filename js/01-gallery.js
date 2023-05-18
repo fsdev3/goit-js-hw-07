a4a699db1a6a2372179ff40c9b68814eeeb85809
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-05-18 19:49:27 +0200

    t1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesGallery = document.querySelector(".gallery");
+
+const getGalleryItems = galleryItems
+  .map(
+    (image) =>
+      `<li class='gallery__item'>
+      <a class='gallery__link' href='${image.original}'>
+      <img class='gallery__image' src='${image.preview}' 
+      data-source='${image.original}'
+      alt='${image.description}'></a></li>`
+  )
+  .join("");
+
+imagesGallery.insertAdjacentHTML("beforeend", getGalleryItems);
